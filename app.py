477a21512794afe1e167a4d58b38467dfa879f4e
Anatoliy <anbrikzone@gmail.com>
2024-01-25 23:23:53 +0500

    add .gitignore

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def index):
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("")
