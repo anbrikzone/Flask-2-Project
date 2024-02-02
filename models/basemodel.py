a6561f1f304c8cf67e96ee2aebc76b1d7d8beb61
Anatoliy <anbrikzone@gmail.com>
2024-02-02 20:39:54 +0500

    Upload

--- models/basemodel.py
+++ models/basemodel.py
@@ -5,21 +5,36 @@ class BaseModel():
     def __init__(self) -> None:
         self.connection = sqlite3.connect(database="database/db.sqlite")
         self.cursor = self.connection.cursor()
-        self.create_table()
+        self.create_table_users()
+        self.create_table_tasks()
 
-    def create_table(self):
+    def create_table_users(self):
         sql = '''
-            create table if not exists employees (
-                id INTEGER PRIMARY KEY,
-                first_name TEXT,
-                last_name TEXT,
-                department TEXT,
-                salary REAL
-            )
+            CREATE TABLE IF NOT EXISTS users (
+            id       INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+            username TEXT,
+            password TEXT,
+            email    TEXT
+        );
             '''
         self.cursor.execute(sql)
         self.connection.commit()
 
+           
+    def create_table_tasks(self):
+        sql = '''
+            CREATE TABLE IF NOT EXISTS tasks (
+            id      INTEGER     PRIMARY KEY AUTOINCREMENT UNIQUE,
+            title   TEXT,
+            status  INTEGER (1),
+            user_id INTEGER     REFERENCES users (id)   ON DELETE CASCADE
+                                                        ON UPDATE CASCADE
+        );
+            '''
+        self.cursor.execute(sql)
+        self.connection.commit()       
+           
+            
     
 
         
